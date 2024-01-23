71df578c2e6c9d140e9c659d5e61390241fc79c6
Laxitasojitra <laxitasojitra00@gmail.com>
2024-01-23 23:58:49 +0530

    update visual.py

--- visualize.py
+++ visualize.py
@@ -1,11 +1,27 @@
-import ast
+# Import libraries
+import ast  # Abstract Syntax Trees module
+import cv2  # OpenCV for computer vision
+import numpy as np  # Numerical computing library
+import pandas as pd  # Data manipulation library
 
-import cv2
-import numpy as np
-import pandas as pd
 
+# Function to draw a border around a rectangle in an image
 
+    # Draws a border around a rectangle in the image.
+
+    # Parameters:
+    # - img: Input image
+    # - top_left: Top-left coordinates of the rectangle
+    # - bottom_right: Bottom-right coordinates of the rectangle
+    # - color: Border color (default is green)
+    # - thickness: Border thickness (default is 10 pixels)
+    # - line_length_x: Length of lines in the x-direction (default is 200 pixels)
+    # - line_length_y: Length of lines in the y-direction (default is 200 pixels)
+
+    # Returns:
+    # - Modified image with borders drawn
 def draw_border(img, top_left, bottom_right, color=(0, 255, 0), thickness=10, line_length_x=200, line_length_y=200):
+    
     x1, y1 = top_left
     x2, y2 = bottom_right
 
@@ -23,40 +39,52 @@ def draw_border(img, top_left, bottom_right, color=(0, 255, 0), thickness=10, li
 
     return img
 
-
+# Read license plate recognition results from a CSV file
 results = pd.read_csv('./test_interpolated.csv')
 
-# load video
+
+# Load the video
 video_path = 'sample.mp4'
 cap = cv2.VideoCapture(video_path)
 
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Specify the codec
+# Video Processing Setup
+# Specify video writing parameters
+fourcc = cv2.VideoWriter_fourcc(*'mp4v') 
 fps = cap.get(cv2.CAP_PROP_FPS)
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 out = cv2.VideoWriter('./out.mp4', fourcc, fps, (width, height))
 
+# License Plate Information Extraction
+# Dictionary to store license plate information for each car ID
 license_plate = {}
+
+
+# Iterate through unique car IDs in the recognition results
 for car_id in np.unique(results['car_id']):
+     # Extract the license plate information with the highest score for each car
     max_ = np.amax(results[results['car_id'] == car_id]['license_number_score'])
     license_plate[car_id] = {'license_crop': None,
                              'license_plate_number': results[(results['car_id'] == car_id) &
                                                              (results['license_number_score'] == max_)]['license_number'].iloc[0]}
+    # Set the video frame to the one corresponding to the highest-scoring license plate
     cap.set(cv2.CAP_PROP_POS_FRAMES, results[(results['car_id'] == car_id) &
                                              (results['license_number_score'] == max_)]['frame_nmr'].iloc[0])
+     # Read the frame
     ret, frame = cap.read()
 
+    # Extract license plate bounding box coordinates
     x1, y1, x2, y2 = ast.literal_eval(results[(results['car_id'] == car_id) &
                                               (results['license_number_score'] == max_)]['license_plate_bbox'].iloc[0].replace('[ ', '[').replace('   ', ' ').replace('  ', ' ').replace(' ', ','))
-
+    # Crop and resize the license plate image
     license_crop = frame[int(y1):int(y2), int(x1):int(x2), :]
     license_crop = cv2.resize(license_crop, (int((x2 - x1) * 400 / (y2 - y1)), 400))
 
     license_plate[car_id]['license_crop'] = license_crop
 
-
+# Video Processing Loop
 frame_nmr = -1
-
+# Set video frame position to the beginning
 cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
 
 # read frames
@@ -64,9 +92,13 @@ ret = True
 while ret:
     ret, frame = cap.read()
     frame_nmr += 1
+    
+     # Check if the frame was successfully read
     if ret:
+        # Extract information for the current frame
         df_ = results[results['frame_nmr'] == frame_nmr]
         for row_indx in range(len(df_)):
+            
             # draw car
             car_x1, car_y1, car_x2, car_y2 = ast.literal_eval(df_.iloc[row_indx]['car_bbox'].replace('[ ', '[').replace('   ', ' ').replace('  ', ' ').replace(' ', ','))
             draw_border(frame, (int(car_x1), int(car_y1)), (int(car_x2), int(car_y2)), (0, 255, 0), 25,
@@ -87,7 +119,8 @@ while ret:
 
                 frame[int(car_y1) - H - 400:int(car_y1) - H - 100,
                       int((car_x2 + car_x1 - W) / 2):int((car_x2 + car_x1 + W) / 2), :] = (255, 255, 255)
-
+                
+                    # Add license plate number as text
                 (text_width, text_height), _ = cv2.getTextSize(
                     license_plate[df_.iloc[row_indx]['car_id']]['license_plate_number'],
                     cv2.FONT_HERSHEY_SIMPLEX,
@@ -104,12 +137,13 @@ while ret:
 
             except:
                 pass
-
+            
+# Write the frame to the output video
         out.write(frame)
         frame = cv2.resize(frame, (1280, 720))
 
         # cv2.imshow('frame', frame)
         # cv2.waitKey(0)
-
+# Release video writing and reading resources
 out.release()
 cap.release()
